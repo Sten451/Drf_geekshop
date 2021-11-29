1759df25a39707d0868af178f6d8b19314cc13ff
Vyacheslav <lvv_1@list.ru>
2021-11-29 18:01:07 +0300

    Ready homework_4

--- project/views.py
+++ project/views.py
@@ -2,13 +2,50 @@ from rest_framework.viewsets import ModelViewSet
 
 from project.models import Project, Todo
 from project.serialiazers import ProjectModelSerializer, TodoModelSerializer
+from rest_framework.pagination import LimitOffsetPagination
+from django_filters import rest_framework as filter
+from rest_framework.response import Response
+from rest_framework import status
 
+
+# Пагинация
+class ProjectPagination(LimitOffsetPagination):
+    default_limit = 10
+
+class TODOPagination(LimitOffsetPagination):
+    default_limit = 20
+
+#фильтрация
+class ProjectFilter(filter.FilterSet):
+    name = filter.CharFilter(lookup_expr="contains")
+
+    class Meta:
+        model = Project
+        fields = ['name']
+
+class TodoFilter(filter.FilterSet):
+
+    class Meta:
+        model = Todo
+        fields = ['project']
 
 class ProjectModelViewSet(ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectModelSerializer
+    pagination_class = ProjectPagination
+    filterset_class = ProjectFilter
 
 
 class TodoModelViewSet(ModelViewSet):
     queryset = Todo.objects.all()
     serializer_class = TodoModelSerializer
+    pagination_class = TODOPagination
+    filterset_class = TodoFilter
+
+    def destroy(self, request, *args, **kwargs):
+        todo3 = self.get_object()
+        if todo3.is_active == True:
+            todo3.is_active = False
+            todo3.save()
+        return Response(status=status.HTTP_202_ACCEPTED)
+
--- users/views.py
+++ users/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import GenericViewSet
 from .models import Users
 from .serialiazers import UsersModelSerializer
+from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, UpdateModelMixin
 
-class UsersModelViewSet(ModelViewSet):
+class UsersModelViewSet(ListModelMixin, RetrieveModelMixin, UpdateModelMixin, GenericViewSet):
     queryset = Users.objects.all()
     serializer_class = UsersModelSerializer
